a574fe31ceb3c433beeb044fc7dc394440dbb694
Mario Gomez <mariogomezmtz@gmail.com>
2016-07-08 00:45:56 -0500

    Update bower dependencies, add gulp tasks

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var stringify = require('stringify');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('connect', function() {
+  connect.server({
+    root: 'public',
+    port: 4000
+  });
+});
+
+gulp.task('browserify', function() {
+  return browserify('./app/app.js', {
+      transform: stringify({
+        extensions: ['.html'],
+        minify: true
+      })
+    })
+    .bundle()
+    .pipe(source('main.js'))
+    .pipe(gulp.dest('./public/js'));
+});
+
+gulp.task('sass', function() {
+  gulp.src('./scss/**/*.{scss,sass}')
+    .pipe(sass({
+      errLogToConsole: true
+    }))
+    .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('watch', ['browserify', 'sass'], function() {
+  gulp.watch('app/**/{*.js,*.html}', ['browserify']);
+  gulp.watch('scss/**/*.{scss,sass}', ['sass']);
+});
+
+gulp.task('default', ['watch', 'connect']);
