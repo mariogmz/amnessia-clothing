2b753a2f6d24e32e8c8181683738532b1dcc3afd
Mario Gomez <mariogomezmtz@gmail.com>
2016-07-30 17:05:17 -0500

    Refactor header for new scroll page@

---
+++ app/header/acHeader.directive.js
@@ -0,0 +1,35 @@
+'use strict';
+
+AcHeader.$inject = [];
+
+function AcHeader() {
+  var directive = {
+    bindToController: true,
+    controller: HeaderController,
+    controllerAs: 'headerVM',
+    template: require('./acHeader.html'),
+    restrict: 'A',
+  }
+
+  return directive;
+}
+
+HeaderController.$inject = [];
+
+function HeaderController() {
+  var vm = this;
+  activate();
+
+  function activate() {
+    vm.navItems = [
+      {label: 'inicio', link: '#home'},
+      {label: 'catálogo', link: '/gallery'},
+      {label: 'marca', link: '#brand'},
+      {label: 'ubicación', link: '#location'},
+      {label: 'contacto', link: '#contact'},
+    ];
+  }
+
+}
+
+module.exports = AcHeader;
--- app/header/index.js
+++ app/header/index.js
@@ -2,4 +2,4 @@
 
 var amnessia = require('angular').module('amnessia.header', []);
 
-amnessia.directive('tlHeader', require('./tlHeader.directive'));
+amnessia.directive('acHeader', require('./acHeader.directive'));
