2a751b42e5aaaff024321c5c150aa497479fc4cf
Mario Gomez <mariogomezmtz@gmail.com>
2016-07-30 17:56:16 -0500

    Added layout module for all the page scrollable content

--- app/app.js
+++ app/app.js
@@ -3,11 +3,11 @@
 var amnessia = require('angular').module('amnessia', [
   'amnessia.core',
   'amnessia.header',
-  'amnessia.home',
-  'amnessia.location'
+  'amnessia.layout',
+  'amnessia.home'
 ]);
 
 require('./core');
 require('./header');
+require('./layout');
 require('./home');
-require('./location');
---
+++ app/layout/index.js
@@ -0,0 +1,4 @@
+'use strict';
+
+var amnessia = require('angular').module('amnessia.layout', []);
+amnessia.config(require('./routes'));
