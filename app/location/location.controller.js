b7a8ed052947cf86d9f83a24b0fafba7349677e5
Mario Gomez <mariogomezmtz@gmail.com>
2016-07-31 12:57:36 -0500

    Add marker and real location to map

--- app/location/location.controller.js
+++ app/location/location.controller.js
@@ -10,8 +10,12 @@ function LocationController() {
   /////////////////////////
 
   function activate() {
-    vm.map = {center: {latitude: 21.67914, longitude: -102.58425 }, zoom: 28 };
+    vm.map = {center: {latitude: 21.6777415, longitude: -102.5852379 }, zoom: 15 };
     vm.options = {scrollwheel: false};
+    vm.marker = {idKey: 'amnessia', coords: {latitude: 21.67914, longitude: -102.58425}, options: {
+      label: 'Amnessia Clothing',
+      title: 'Amnessia Clothing'
+    }};
   }
 }
 
