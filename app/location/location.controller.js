2d9e64c945f9d6dc36cc83a5af7778545e7d7a2a
Mario Gomez <mariogomezmtz@gmail.com>
2016-07-31 23:47:39 -0500

    Update location feed and html markup with location data

--- app/location/location.controller.js
+++ app/location/location.controller.js
@@ -10,12 +10,21 @@ function LocationController() {
   /////////////////////////
 
   function activate() {
+    setMapSettings();
+    setLocationData();
+  }
+
+  function setMapSettings() {
     vm.map = {center: {latitude: 21.6777415, longitude: -102.5852379 }, zoom: 15 };
     vm.options = {scrollwheel: false};
     vm.marker = {idKey: 'amnessia', coords: {latitude: 21.67914, longitude: -102.58425}, options: {
       label: 'Amnessia Clothing',
       title: 'Amnessia Clothing'
     }};
+  }
+
+  function setLocationData() {
+    vm.locationData = require('../data/location.json');
   }
 }
 
