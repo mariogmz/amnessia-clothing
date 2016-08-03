4f7ad91f7be01ea2a2b9eba6c8a618e73a4931ff
Mario Gomez <mariogomezmtz@gmail.com>
2016-08-03 02:27:38 -0500

    Remove Slick

--- app/brand/brand.controller.js
+++ app/brand/brand.controller.js
@@ -5,17 +5,6 @@ function BrandController() {
   activate();
 
   function activate() {
-    vm.slickConfig = {
-      enabled: true,
-      autoplay: true,
-      draggable: false,
-      autoplaySpeed: 3000,
-      method: {},
-      event: {
-          beforeChange: function (event, slick, currentSlide, nextSlide) {},
-          afterChange: function (event, slick, currentSlide, nextSlide) {}
-      }
-    };
   }
 }
 
--- app/core/index.js
+++ app/core/index.js
@@ -6,16 +6,13 @@ require('angular-local-storage');
 require('angular-simple-logger');
 require('angular-google-maps');
 require('angular-scroll');
-require('slick-carousel');
-require('angular-slick-carousel');
 require('../blocks');
 
 var amnessia = require('angular').module('amnessia.core', [
   'ui.router',
   'LocalStorageModule',
   'uiGmapgoogle-maps',
-  'duScroll',
-  'slickCarousel'
+  'duScroll'
 ]).value('duScrollOffset', 162);
 
 amnessia.config( require('./config') );
