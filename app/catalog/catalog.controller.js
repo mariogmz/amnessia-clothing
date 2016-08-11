841ff075ac404fb137971f891ae17f42b8e3fc01
Mario Gomez <mariogomezmtz@gmail.com>
2016-08-11 00:50:20 -0500

    Add masonry to project

--- app/catalog/catalog.controller.js
+++ app/catalog/catalog.controller.js
@@ -1,11 +1,21 @@
-CatalogController.$inject = [];
+CatalogController.$inject = ['$timeout'];
 
-function CatalogController() {
+function CatalogController($timeout) {
   var vm = this;
   activate();
 
   function activate() {
     vm.gallery = require('../data/gallery.json');
+    jQuery(window).on('load', function($){
+      initializeMasonry();
+    });
+  }
+
+  function initializeMasonry() {
+    var Masonry = require('masonry-layout');
+    var masonry = new Masonry('.catalog__gallery', {
+      itemSelector: '.catalog__gallery--item'
+    });
   }
 }
 
