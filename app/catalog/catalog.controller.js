d3b9c8a7b770a8ee55401d53f4f5791d7e16368a
Mario Gomez <mariogomezmtz@gmail.com>
2016-08-11 21:37:00 -0500

    Refactor controller to initialize masonry after images are downloaded

--- app/catalog/catalog.controller.js
+++ app/catalog/catalog.controller.js
@@ -6,27 +6,27 @@ function CatalogController($scope) {
 
   function activate() {
     vm.gallery = require('../data/gallery.json');
-    bindEvents();
+    $(document).ready(initializeMasonry);
   }
 
-  function bindEvents() {
-    $scope.$on('$stateChangeSuccess', function (event, toState) {
-      if(toState.name == 'catalog'){
-        waitForImagesToLoad();
-      }
-    });
-  }
-
-  function waitForImagesToLoad() {
+  function initializeMasonry() {
     var imagesLoaded = require('imagesloaded');
-    imagesLoaded('.catalog__gallery', initializeMasonry);
-  }
+    var gallery = document.querySelector('.catalog__gallery');
+    new imagesLoaded(gallery, function(instance) {
+      var Masonry = require('masonry-layout');
+      var masonry = new Masonry('.catalog__gallery', {
+        itemSelector: '.catalog__gallery--item'
+      });
+    }).on('always', always);
 
-  function initializeMasonry(instance) {
-    var Masonry = require('masonry-layout');
-    var masonry = new Masonry('.catalog__gallery', {
-      itemSelector: '.catalog__gallery--item'
-    });
+    function always(instance) {
+      var Masonry = require('masonry-layout');
+      var masonry = new Masonry('.catalog__gallery', {
+        itemSelector: '.catalog__gallery--item'
+      });
+    }
+
+
   }
 
 }
